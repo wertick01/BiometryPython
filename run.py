a0c73854bcd4ebf54891df83b6205dfc1024c88a
Pavel Desyukevich <desyukevich.pyu@dclouds.ru>
2023-06-17 20:53:35 +0700

    Smth

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from counter import Counter
 from saver import Saver
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
 
 class Run(object):
     def __init__(self, inputFilePath, outputDirPath, sep):
@@ -21,34 +22,15 @@ class Run(object):
 
     def getAllResults(self):
         self.counter.getStatisticsFrom1To11()
-        # print(self.counter.from1To11DF)
-        # print()
         self.counter.getStatisticsFrom12To18()
-        # print(self.counter.from12To18DF)
-        # print()
         self.counter.getStatisticsFrom19To20()
-        # print(self.counter.from19To20DF)
-        # print()
         self.counter.getStatisticsFrom22()
-        # print(self.counter.spearman)
-        # print()
         self.counter.getStatisticsFrom23To26()
-        # print(self.counter.from23To26DF)
-        # print()
         self.counter.getStatisticsFrom27To28()
-        # print(self.counter.from27To28DF)
-        # print()
         self.counter.getStatisticsFrom29To31()
-        # print(self.counter.from29To31DF)
-        # print()
         self.counter.getStatisticsFrom32To33()
-        # print(self.counter.from32To33DF)
-        # print()
         self.counter.getStatisticsFrom34To36()
-        # print(self.counter.from34To36DF)
-        # print()
         self.counter.formatToFinalLine(name=self.name)
-        # print(self.counter.finalLine)
 
     def saveFiles(self):
         self.saver.save(dataframe=self.counter.from1To11DF, numbers="_1_to_11", name=self.name)
@@ -85,14 +67,19 @@ def run_script():
 if __name__ == "__main__":
     root = tk.Tk()
 
+    label_style = ttk.Style(root)
+    label_style.configure(
+        ".",          
+        font="helvetica 14",   
+        foreground="#004D40",   
+        padding=10,            
+        background="#B2DFDB",
+    )
+
     # кнопки для запуска программы и выбора файлов
     run_button = tk.Button(root, text="Run Script", command=run_script)
-    open_button = tk.Button(root, text="Open File", command=open_file)
-    save_button = tk.Button(root, text="Save File", command=save_file)
 
     # размещение кнопок на экране
-    open_button.pack(side=tk.LEFT)
     run_button.pack(side=tk.LEFT)
-    save_button.pack(side=tk.LEFT)
 
     root.mainloop()
